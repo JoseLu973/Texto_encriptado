e3ec18673800b2ddc246934e9db892489312b8b2
JoseLu973 <134983628+JoseLu973@users.noreply.github.com>
2024-08-28 01:27:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function encryptText() {
+    let text = document.getElementById("inputText").value;
+
+    let encryptedText = text.replace(/e/g, "enter")
+                            .replace(/i/g, "imes")
+                            .replace(/a/g, "ai")
+                            .replace(/o/g, "ober")
+                            .replace(/u/g, "ufat");
+
+    document.getElementById("outputText").value = encryptedText;
+}
+
+function decryptText() {
+    let text = document.getElementById("inputText").value;
+
+    let decryptedText = text.replace(/enter/g, "e")
+                            .replace(/imes/g, "i")
+                            .replace(/ai/g, "a")
+                            .replace(/ober/g, "o")
+                            .replace(/ufat/g, "u");
+
+    document.getElementById("outputText").value = decryptedText;
+}
+
+function copyText() {
+    let textToCopy = document.getElementById("outputText");
+    textToCopy.select();
+    document.execCommand("copy");
+    alert("Texto copiado al portapapeles!");
+}
